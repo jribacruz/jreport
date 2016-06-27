632dac5d4e4c4c89ddeda20ef1d1dc23df40a900
Leonardo <leosr89@gmail.com>
2016-06-27 10:05:34 -0300

    11

--- jreport/src/test/java/br/jreport/model/Teste2.java
+++ jreport/src/test/java/br/jreport/model/Teste2.java
@@ -1,5 +1,10 @@
 package br.jreport.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.jreport.table.DataTableBodyModel;
+import br.jreport.template.DataTable;
 import br.jreport.template.SimpleReport;
 
 public class Teste2 extends SimpleReport {
@@ -20,6 +25,26 @@ public class Teste2 extends SimpleReport {
 
 	@Override
 	protected void detail() {
+		List<ModelTest> list = new ArrayList<ModelTest>();
+		ModelTest test = new ModelTest();
+		test.setNome("teste1");
+		test.setSobrenome("sobrenome1");
+		ModelTest test2 = new ModelTest();
+		test2.setNome("teste2");
+		test2.setSobrenome("sobrenome2");
+		list.add(test);
+		list.add(test2);
+		addDataTable(new DataTable().addCell("teste1").addCell("sobrenome1").addCell("teste2").addCell("sobrenome2"));
+		
+		addDataTable(new DataTable().addDataTableBody(list, new DataTableBodyModel<ModelTest>() {
+
+			@Override
+			public void body(ModelTest model) {
+				addCell(model.getNome());
+				addCell(model.getSobrenome());
+				
+			}
+		}).addCell(""));
 	}
 
 	@Override
