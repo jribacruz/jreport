a18836f40c5accbc9b2ad2ae4a7045b15c12f5f0
Leonardo <leosr89@gmail.com>
2016-06-27 09:44:07 -0300

    ajuste

--- jreport/src/test/java/br/jreport/executaveis/ReportApp.java
+++ jreport/src/test/java/br/jreport/executaveis/ReportApp.java
@@ -4,16 +4,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import br.jreport.NovoRelatorioSimpleReport;
-import br.jreport.ReportSecoes;
-import br.jreport.ReportTextoSimples;
-import br.jreport.model.ModelTest;
+import br.jreport.model.Teste2;
 
 public class ReportApp {
 
 	public static void main(String[] args) throws IOException {
-		NovoRelatorioSimpleReport report1 = new NovoRelatorioSimpleReport();
-		report1.generate(new FileOutputStream(new File("target/NovoRelatorioSimpleReport.pdf")));
+		Teste2 report1 = new Teste2();
+		report1.generate(new FileOutputStream(new File("target/teste2.pdf")));
 		
 	}
 
--- jreport/src/test/java/br/jreport/model/Teste2.java
+++ jreport/src/test/java/br/jreport/model/Teste2.java
@@ -1,9 +1,6 @@
 package br.jreport.model;
 
-import br.jreport.table.DataTableBodyModel;
-import br.jreport.template.Component;
 import br.jreport.template.SimpleReport;
-import br.jreport.template.Title;
 
 public class Teste2 extends SimpleReport {
 
@@ -14,44 +11,24 @@ public class Teste2 extends SimpleReport {
 
 	@Override
 	protected void title() {
-//		addTitle(new Title(getDocument()).T_addBrasao().T_addTitle("Texto").T_addTitle("teste").T_addTitle("teste2"));
-//		addDataTable(new DataTable<ModelTest>(document).addDataTable(null, 0, new DataTableBody<ModelTest>() {
-//			
-//			@Override
-//			public void body(ModelTest model) {
-//				
-//			}
-//		}));
-//		addDataTable(new DataTable<ModelTest>(getDocument()).addDataTable(null, 0, new DataTableBody<ModelTest>() {
-//
-//			@Override
-//			public void body(ModelTest model) {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//		}));
 	}
 
 	@Override
 	protected void pageHeader() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void detail() {
-		addComponent(new Component().C_addText("FUncionalidade"));
 	}
 
 	@Override
 	protected void pageFooter() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void summary() {
-		// TODO Auto-generated method stub
 
 	}
 
