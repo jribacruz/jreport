eaa046d2b441cce03cbb2fb67f59a1e774efbaaf
Leonardo <leosr89@gmail.com>
2016-08-09 14:17:55 -0300

    v 0.1.1

--- jreport/src/main/java/br/jreport/template/SimpleReport.java
+++ jreport/src/main/java/br/jreport/template/SimpleReport.java
@@ -156,7 +156,7 @@ public abstract class SimpleReport extends PdfPageEventHelper implements Seriali
 		font.setColor(Color.DARK_GRAY);
 		float xRight = document.right();
 		float xLeft = document.left() + document.leftMargin();
-		float y = document.bottom()-10;
+		float y = document.bottom()-15;
 		String now = new SimpleDateFormat("dd/MM/yyyy HH:ss").format(new Date());
 
 		ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_CENTER, new Phrase(String.format("%d", pagenumber), font),
